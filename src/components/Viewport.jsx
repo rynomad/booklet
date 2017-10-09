69e494e82bb04ec0fc64436683e8ab756888c096
Ryan Bennett <ryan@filament.com>
2017-10-08 22:55:52 -0600

    basic viewport

--- src/components/Viewport.jsx
+++ src/components/Viewport.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Carousel, CarouselItem } from 'react-onsenui'
 import { observer } from 'mobx-react'
+import StaticPage from './StaticPage'
+import Essay from './Essay'
+
 import 'onsenui/css/onsenui.css'
 import 'onsenui/css/onsen-css-components.css'
 import '@blueprintjs/core/dist/blueprint.css'
+
+const ViewPage = observer(({store}) => {
+  switch (store.type){
+    case 'static_page': return <StaticPage store={store}/>
+    case 'essay' : return <Essay store={store}/>
+    default : throw new Error(`no component for type ${store.type}`)
+  }
+})
 
 const Viewport = observer(({store}) => (
   <Carousel index={store.viewIndex} onOverscroll={store.onViewOverscroll} onPostChange={store.onViewPostChange} fullscreen swipeable autoScroll overscrollable>
@@ -14,7 +25,7 @@ const Viewport = observer(({store}) => (
           alignItems: "center",
           justifyContent: "center"
         }}>
-          <div>{store.title}</div>
+          <ViewPage store={store}/>
         </CarouselItem>
       )
     }
