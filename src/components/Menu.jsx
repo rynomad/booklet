570dc72decd18a95dc269df2fab84e25292887f5
Ryan Bennett <ryan@filament.com>
2017-10-08 19:44:23 -0600

    use correct props for menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {Collapse} from "@blueprintjs/core"
-import {List, ListItem} from 'react-onsenui'
+import {List, ListItem, Icon} from 'react-onsenui'
 import {observer} from 'mobx-react'
 
 const MenuItem = observer(({store}) => (
   <Collapse isOpen={store.menuIsOpen}>
     <ListItem 
       onClick={store.onMenuSelect} 
-      style={{background : store.background}}>
-      {store.menuIcon}
+      style={{background : store.menuBackground}}>
+      <Icon style={store.menuIconStyle} icon={store.menuIcon}/>
       {store.title}
     </ListItem>
   </Collapse>
