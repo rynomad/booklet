1ab86d2a1e8b1a1ba2e210ff999417470bbb9597
Ryan Bennett <ryan@filament.com>
2017-10-09 20:47:58 -0600

    remove dead code

--- src/components/StaticPage.jsx
+++ src/components/StaticPage.jsx
@@ -4,12 +4,11 @@ import {Card, Row} from 'react-onsenui'
 import 'onsenui/css/onsenui.css'
 import 'onsenui/css/onsen-css-components.css'
 
-const StaticPage = observer((props, a2, a3) => {
-  console.log(props, a2, a3);  
+const StaticPage = observer(({store}) => {
   return (
     <Card style={{verticalAlign: 'middle', flex : "1", padding: "1em", margin: "2em"}}>
     {
-      props.store.text.map((text, index) => (
+      store.text.map((text, index) => (
         <Row key={index} style={{justifyContent : 'center'}}>
           <h5 style={{align : 'center'}}>{text}</h5>
         </Row>
--- src/stores/any/index.js
+++
@@ -1,9 +0,0 @@
-import {types} from 'mobx-state-tree'
-
-import {section, _section} from '../section'
-import {static_page, _static_page} from '../static_page'
-import {essay, _essay, note} from '../essay'
-const any = types.late(() => types.union(section, static_page, essay, note))
-const _any = types.late(() => types.union(_section, _static_page, _essay))
-console.log(any, _any)
-export { any, _any }
